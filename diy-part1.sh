60cdfda41f19eb6b9b7eb2631ed9dc69522f4e21
Joelyi <37099326+Joelyi@users.noreply.github.com>
2021-05-16 22:15:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,8 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# Define My Package
+# 添加 Package
 #git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
 #git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
+git clone https://github.com/Joelyi/luci-app-ssr-plus.git                            package/Joelyi/luci-app-ssr-plus
